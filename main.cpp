687695a91fea33bb9839a0928021865e825b5815
Jonathan_brahmi <97665994+jonathanbrahmi@users.noreply.github.com>
2022-11-16 05:49:51 -0800

    main.cpp
    The plus operator is written in the language

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream> 
+
+using namespace std; 
+
+int main() 
+{
+  int x, y; 
+  string stop;
+  cout <<"Enter a digit \n"; 
+  cin >>x; 
+  cout <<"Enter a digit again\n"; 
+  sum = x + y; 
+  cout <<"result = "<<sum; 
+  getline(cin, stop); 
+  return 0;
+}
